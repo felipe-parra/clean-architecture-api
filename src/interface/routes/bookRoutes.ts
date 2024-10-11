03ed394fd934c7f985cd56c48aae04e036affe8a
Felipe Parra <fpr29.10@gmail.com>
2024-10-11 16:35:02 -0600

    ✨ (bookRoutes.ts): update bookRoutes to require authentication before accessing routes for fetching all books and creating a new book.
    🔧 (bookRoutes.ts): fix the missing semicolon in line 6 to maintain consistency and prevent potential errors.

--- src/interface/routes/bookRoutes.ts
+++ src/interface/routes/bookRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { BookController } from "../controllers/BookController";
+import { authenticateToken } from "../middleware/auth";
 
-
-const router = Router()
+const router = Router();
 
 const bookController = new BookController();
 
-router.get('/', bookController.getAll);
+router.get('/', authenticateToken, (req, res) => bookController.getAll(req, res));
+router.post('/', authenticateToken, (req, res) => bookController.create(req, res));
 
-export {router as bookRoutes};
+export { router as bookRoutes };
