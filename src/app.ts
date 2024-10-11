874f7f8c35bbb8e4d76b52c4b12db8c714e879e1
Felipe Parra <fpr29.10@gmail.com>
2024-10-11 17:34:41 -0600

    📝 (app.ts): add dotenv package to load environment variables from .env file 🚀 (app.ts): log server startup message to provide feedback when server is running

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import { bookRoutes } from './interface/routes/bookRoutes';
 import { errorHandler } from "./interface/middleware/errorHandler";
 import { logger } from "./infrastructure/logger";
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an Express app
 const app = express()
 
+// Set JSON parser middleware
 app.use(express.json());
 
+// Mount the book routes
 app.use("/api/books",bookRoutes);
 
+// Set error handler middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT ?? 4000;
 
 app.listen(PORT, () => {
+  // Log server startup message
   logger.info("Server is running on port 3000");
 });
